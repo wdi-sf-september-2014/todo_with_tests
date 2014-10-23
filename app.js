5f3d5536544ea9bbf1ca0080c1b46ca7393ce331
Alex Notov <anotov@gmail.com>
2014-10-23 11:11:35 -0700

    add routes for app

--- app.js
+++ app.js
@@ -16,4 +16,36 @@ app.use(methodOverride("_method"));
 
 app.use(express.static(__dirname + '/public'));
 
+app.get('/', function(req, res) {
+  res.redirect('/lists');
+});
+
+app.get('/lists', function(req, res) {
+  
+});
+
+app.post('/lists', function(req, res) {
+
+});
+
+app.delete('/lists/:id', function(req, res) {
+
+});
+
+app.get('/lists/:id/todos', function(req, res) {
+
+});
+
+app.post('/lists/:id/todos', function(req, res) {
+
+});
+
+app.put('/todos/:id', function(req, res) {
+
+});
+
+app.delete('/todos/:id', function(req, res) {
+
+});
+
 app.listen(3000);
