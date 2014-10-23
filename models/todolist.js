fb71622c7cb3c951581c73acabd90dee497a7011
Alex Notov <anotov@gmail.com>
2014-10-22 19:48:57 -0700

    Passing test

--- models/todolist.js
+++ models/todolist.js
@@ -2,7 +2,12 @@
 
 module.exports = function(sequelize, DataTypes) {
   var TodoList = sequelize.define("TodoList", {
-    title: DataTypes.STRING,
+    title: {
+      type: DataTypes.STRING,
+      validate: {
+        len: [0, 100] 
+      }
+    },
     description: DataTypes.STRING
   }, {
     classMethods: {
